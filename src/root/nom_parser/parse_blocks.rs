394bd93130820ab480d56b44aad1ee27c95ebe63
Robert Lucas <robert01.lucas@protonmail.com>
2024-04-11 10:54:19 +0100

    Continued nom parser implementation

--- src/root/nom_parser/parse_blocks.rs
+++ src/root/nom_parser/parse_blocks.rs
@@ -12,23 +12,26 @@ use crate::root::nom_parser::parse_util::discard_ignored;
 
 // ! BROKEN
 
-const DEFAULT_TERMINATORS: [(char, char); 2] = [
-    ('{', '}'),
-    ('(', ')')
+const DEFAULT_TERMINATORS: [(char, char, bool); 3] = [
+    ('{', '}', true),
+    ('(', ')', true),
+    ('"', '"', false)
 ];
 
 pub fn default_section(s: Span, section_char: char) -> ParseResult {
     let x = section(
         s,
-        DEFAULT_TERMINATORS.iter().find_position(|(c, _)| *c == section_char).unwrap().0,
+        DEFAULT_TERMINATORS.iter().find_position(|(c, _, _)| *c == section_char).unwrap().0,
         &DEFAULT_TERMINATORS
     );
     // println!("----------\n{:?}\n**{}\n\n**{}\n\n**{}", section_char, s.fragment(), x.as_ref().unwrap().0.fragment(), x.as_ref().unwrap().1.fragment());
     x
 }
 
-pub fn section<'a>(s: Span<'a>, terminator: usize, all_terminators: &[(char, char)]) -> ParseResult<'a> {
+pub fn section<'a>(s: Span<'a>, terminator: usize, all_terminators: &[(char, char, bool)]) -> ParseResult<'a> {
     let (initial_span, _) = nchar(all_terminators[terminator].0)(s)?;
+
+    let allow_subsections = all_terminators[terminator].2;
 
     let mut depth = 0;
 
@@ -44,8 +47,7 @@ pub fn section<'a>(s: Span<'a>, terminator: usize, all_terminators: &[(char, cha
         if let Ok((ns, _)) = nchar::<_, ErrorTree>(all_terminators[terminator].1)(s) {
             if depth == 0 {
                 return Ok((ns, initial_span.take_split(initial_span.offset(&s)).1));
-            }
-            else {
+            } else {
                 s = ns;
                 depth -= 1;
                 continue;
@@ -58,21 +60,23 @@ pub fn section<'a>(s: Span<'a>, terminator: usize, all_terminators: &[(char, cha
             continue;
         }
 
-        for (pos, t) in all_terminators.iter().enumerate() {
-            if pos == terminator { continue; }
+        if allow_subsections {
+            for (pos, t) in all_terminators.iter().enumerate() {
+                if pos == terminator { continue; }
 
-            if let Ok(_) = nchar::<_, ErrorTree>(t.0)(s) {
-                s = section(s, pos, all_terminators)?.0;
-                continue 'main;
+                if let Ok(_) = nchar::<_, ErrorTree>(t.0)(s) {
+                    s = section(s, pos, all_terminators)?.0;
+                    continue 'main;
+                }
+
+                if let Ok(_) = nchar::<_, ErrorTree>(t.1)(s) {
+                    // Unopened section closed
+                    todo!()
+                }
             }
 
-            if let Ok(_) = nchar::<_, ErrorTree>(t.1)(s) {
-                // Unopened section closed
-                todo!()
-            }
+            s = anychar(s)?.0;
         }
-
-        s = anychar(s)?.0;
     }
 
     Err(Error(ErrorTree::from_char(s, all_terminators[terminator].1)))
