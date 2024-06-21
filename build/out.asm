d896fefb57329cb30b64ff5bf794e49f505f124e
Robert-M-Lucas <robert-m-lucas@example.com>
2024-06-21 11:05:02 +0100

    Tests

--- build/out.asm
+++ build/out.asm
@@ -5,49 +5,44 @@ section .text
 main:
     push rbp
     mov rbp, rsp
-    mov qword [rbp-8], 1
+    mov qword [rbp-8], 0
+    main_0:
     mov rax, qword [rbp-8]
-    mov qword [rbp-16], rax
-    sub rsp, 16
-    call _2
-    add rsp, 16
-    mov qword [rbp-16], 1
-    mov rax, qword [rbp-16]
+    mov qword [rbp-17], rax
+    mov qword [rbp-25], 1000000000
+    mov rax, qword [rbp-25]
+    cmp rax, qword [rbp-17]
+    jg __18_2
+    mov byte [rbp-9], 0
+    jmp __18_3
+    __18_2:
+    mov byte [rbp-9], 1
+    __18_3:
+    cmp byte [rbp-9], 0
+    jz main_1
+    mov rax, rbp
+    add rax, -8
+    mov qword [rbp-33], rax
+    mov qword [rbp-41], 1
+    mov rax, qword [rbp-33]
+    mov rdx, qword [rbp-41]
+    add qword [rax], rdx
+    jmp main_0
+    main_1:
+    mov rax, qword [rbp-8]
+    mov qword [rbp-33], rax
+    mov rdi, __8_fstr
+    mov rsi, [rbp-33]
+    mov al, 0
+    sub rsp, 33
+    extern printf
+    call printf
+    add rsp, 33
+    mov qword [rbp-41], 1
+    mov rax, qword [rbp-41]
     leave
     ret
 
-
-_2:
-    push rbp
-    mov rbp, rsp
-    mov rax, qword [rbp+16]
-    mov qword [rbp-8], rax
-    mov rdi, __8_fstr
-    mov rsi, [rbp-8]
-    mov al, 0
-    sub rsp, 8
-    extern printf
-    call printf
-    add rsp, 8
-    mov qword [rbp-16], 12
-    mov qword [rbp-24], 12
-    mov rax, rbp
-    add rax, 16
-    mov qword [rbp-32], rax
-    mov qword [rbp-40], 1
-    mov rax, qword [rbp-32]
-    mov rdx, qword [rbp-40]
-    add qword [rax], rdx
-    mov rax, qword [rbp+16]
-    mov qword [rbp-48], rax
-    mov rax, qword [rbp-48]
-    mov qword [rbp-56], rax
-    sub rsp, 56
-    call _2
-    add rsp, 56
-
-leave
-ret
 
 section .data_readonly
     __8_fstr db `Integer: %ld\n`,0
