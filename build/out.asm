f39ebc39f3c1966f09766fcb00b4c79886954a55
Robert-M-Lucas <robert-m-lucas@example.com>
2024-07-27 16:56:28 +0100

    Meta

--- build/out.asm
+++ build/out.asm
@@ -5,34 +5,45 @@ section .text
 main:
     push rbp
     mov rbp, rsp
-    mov qword [rbp-8], 13
-    mov qword [rbp-32], 190
-    mov qword [rbp-24], 6
-    mov qword [rbp-16], 8
+    mov qword [rbp-16], 3
+    mov qword [rbp-8], 4
     mov rax, rbp
-    add rax, -32
+    add rax, -16
+    mov qword [rbp-24], rax
+    mov rax, qword [rbp-24]
+    mov qword [rbp-32], rax
+    sub rsp, 32
+    call _3
+    add rsp, 32
+    mov rax, rbp
+    add rax, -16
+    mov qword [rbp-32], rax
+    mov rax, qword [rbp-32]
+    mov qword [rbp-40], rax
+    sub rsp, 40
+    call _4
+    add rsp, 40
+    mov rax, rbp
+    add rax, -16
     mov qword [rbp-40], rax
     mov rax, qword [rbp-40]
     mov qword [rbp-48], rax
     sub rsp, 48
-    call _4
+    call _3
     add rsp, 48
-    mov rax, rbp
-    add rax, -8
-    mov qword [rbp-48], rax
-    mov rax, qword [rbp-24]
+    mov rax, qword [rbp-16]
     mov qword [rbp-56], rax
-    mov rax, qword [rbp-48]
-    mov rdx, qword [rbp-56]
-    add qword [rax], rdx
     mov rax, qword [rbp-8]
     mov qword [rbp-64], rax
-    mov rax, qword [rbp-64]
+    mov rax, qword [rbp-56]
+    add rax, qword [rbp-64]
+    mov qword [rbp-48], rax
+    mov rax, qword [rbp-48]
     leave
     ret
 
 
-_4:
+_3:
     push rbp
     mov rbp, rsp
     mov rdx, qword [rbp+16]
@@ -46,6 +57,38 @@ _4:
     extern printf
     call printf
     add rsp, 8
+    mov rdx, qword [rbp+16]
+    add rdx, 8
+    mov rax, qword [rdx+0]
+    mov qword [rbp-16], rax
+    mov rdi, __8_fstr
+    mov rsi, [rbp-16]
+    mov al, 0
+    sub rsp, 16
+    extern printf
+    call printf
+    add rsp, 16
+
+leave
+ret
+
+_4:
+    push rbp
+    mov rbp, rsp
+    mov rax, rbp
+    add rax, 16
+    mov qword [rbp-8], rax
+    mov qword [rbp-16], 1
+    mov rax, qword [rbp-8]
+    mov rdx, qword [rbp-16]
+    add qword [rax], rdx
+    mov rax, rbp
+    add rax, 24
+    mov qword [rbp-24], rax
+    mov qword [rbp-32], 1
+    mov rax, qword [rbp-24]
+    mov rdx, qword [rbp-32]
+    add qword [rax], rdx
 
 leave
 ret
